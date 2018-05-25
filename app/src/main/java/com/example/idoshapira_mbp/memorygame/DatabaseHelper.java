aa5890825529d65ae918499e715d2998bc9a1007
IdoShapira <ishapira@yotpo.com>
2018-05-25 17:39:39 +0300

    Database helper

---
+++ app/src/main/java/com/example/idoshapira_mbp/memorygame/DatabaseHelper.java
@@ -0,0 +1,88 @@
+package com.example.idoshapira_mbp.memorygame;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by idoshapira-mbp on 25/05/2018.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DatabaseHelper";
+    private static final String TABLE_NAME = "people_score";
+    private static final String COL0 = "ID";
+    private static final String COL1 = "name";
+    private static final String COL2 = "diff";
+    private static final String COL3 = "score";
+
+    public DatabaseHelper(Context context) {
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String dropTable = "DROP IF TABLE EXISTS "+TABLE_NAME;
+        db.execSQL(dropTable);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE "+TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, "+ COL1+" TEXT, "+COL2+" DOUBLE, "+COL3+" INTEGER)";
+        db.execSQL(createTable);
+
+    }
+
+
+
+    public boolean addData(String name,int diff,double score){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL1,name);
+        contentValues.put(COL2,diff);
+        contentValues.put(COL3,score);
+
+        Log.d(TAG, "addData: Adding "+name+" to "+TABLE_NAME);
+
+        long result = db.insert(TABLE_NAME,null,contentValues);
+
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM "+TABLE_NAME;
+        Cursor data = db.rawQuery(query,null);
+        return data;
+    }
+
+    public Cursor getEasyTable(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM "+TABLE_NAME+" WHERE diff=0";
+        Cursor data = db.rawQuery(query,null);
+        return data;
+    }
+
+    public Cursor getMediumTable(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM "+TABLE_NAME+" WHERE diff=1";
+        Cursor data = db.rawQuery(query,null);
+        return data;
+    }
+
+    public Cursor getHardTable(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM "+TABLE_NAME+" WHERE diff=2";
+        Cursor data = db.rawQuery(query,null);
+        return data;
+    }
+}
--- app/src/main/java/com/example/idoshapira_mbp/memorygame/FragmentListScore.java
+++ app/src/main/java/com/example/idoshapira_mbp/memorygame/FragmentListScore.java
@@ -1,6 +1,7 @@
 package com.example.idoshapira_mbp.memorygame;
 
 import android.app.Fragment;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,8 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.List;
 
 /**
  * Created by idoshapira-mbp on 25/05/2018.
@@ -20,18 +19,22 @@ public class FragmentListScore extends Fragment {
     String countries[] = {"Austria","Belgium","Israel"};
 
 
-
     public FragmentListScore(){
         //required empty constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
 
+
+
         View view = inflater.inflate(R.layout.score_list_fragment,container,false);
 
         ListView lv = (ListView)view.findViewById(R.id.listScores);
+
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,countries);
         lv.setAdapter(adapter);
 
